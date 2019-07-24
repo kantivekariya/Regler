d9b3394fa00a1af31b3b185a656ccdf03e565f57
Kanti Vekariya <kantivekariya242@gmail.com>
2019-07-24 22:55:15 +0530

    initial commit

--- src/app/views/profile/profile.component.spec.ts
+++ src/app/views/profile/profile.component.spec.ts
@@ -1,3 +1,25 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:e9644bdf63d98906262a706d34746a26eed14131a38341089e5adbc375802749
-size 635
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfileComponent } from './profile.component';
+
+describe('ProfileComponent', () => {
+  let component: ProfileComponent;
+  let fixture: ComponentFixture<ProfileComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ProfileComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProfileComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
